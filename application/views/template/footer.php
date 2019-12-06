cee12367f197865b462611b9ae625631b45a20d6
mwauragitonga <mwauragitonga12@gmail.com>
2019-12-06 10:12:10 +0300

    header and footer design

---
+++ application/views/template/footer.php
@@ -0,0 +1,77 @@
+<footer class="footer-area section-gap">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h4 class="text-white">About</h4>
+					<p>
+						CallMetron is a revolutionary product that allows you to discover the value of the data in your organization's call logs.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h4 class="text-white">Contact Us</h4>
+					<p>
+						Ready to join us? Drop us a line anytime!
+					</p>
+					<p class="number">
+						012-6532-568-9746 <br>
+						012-6532-569-9748
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-5  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h4 class="text-white">GET A QUOTE</h4>
+					<p>Need us to drop you a quick quote?Leave your email and we will get back to you ASAP.</p>
+					<div class="d-flex flex-row" id="mc_embed_signup">
+
+
+						<form class="navbar-form" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+							<div class="input-group add-on">
+								<input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required="" type="email">
+								<div style="position: absolute; left: -5000px;">
+									<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+								</div>
+								<div class="input-group-btn">
+									<button class="genric-btn"><span class="lnr lnr-arrow-right"></span></button>
+								</div>
+							</div>
+							<div class="info mt-20"></div>
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+<!--						<p class="footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>-->
+			<div class="footer-social d-flex align-items-center">
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- End footer Area -->
+
+<script src="<?php echo base_url(); ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="<?php echo base_url(); ?>assets/js/vendor/bootstrap.min.js"></script>
+<script type="<?php echo base_url(); ?>assets/text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+<script src="<?php echo base_url(); ?>assets/js/easing.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/hoverIntent.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/superfish.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.ajaxchimp.min.js"></script>
+<script src=<?php echo base_url(); ?>assets/"js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.sticky.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.nice-select.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/waypoints.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.counterup.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/parallax.min.js"></script>
+<script src=<?php echo base_url(); ?>assets/"js/mail-script.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+</body>
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,54 @@
+<?php
+header('Cache-Control: max-age=900');
+?>
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="icon" href="<?php echo base_url(); ?>assets/images/icon.icon" type="image/icon" />
+	<!-- Author Meta -->
+	<meta name="author" content="U70XDN">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="Call Manager| Call Logger | Reports">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>CallMetron | Call Manager</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+	<!--
+	CSS
+	============================================= -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/linearicons.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/nice-select.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+</head>
+<body>
+
+<header id="header" id="home">
+	<div class="container">
+		<div class="row align-items-center justify-content-between d-flex">
+			<div id="logo">
+				<a href="<?php echo base_url(); ?>home"><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="" title="" /></a>
+			</div>
+			<nav id="nav-menu-container">
+				<ul class="nav-menu">
+					<li class="menu-active"><a href="#home">Home</a></li>
+					<li><a href="#feature">Features</a></li>
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+			</nav>
+			<!-- #nav-menu-container -->
+		</div>
+	</div>
+</header>
+<!-- #header -->
