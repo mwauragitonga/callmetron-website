1335d9a285c88bbf6a045addf2b53979e3114af1
mwauragitonga <mwauragitonga12@gmail.com>
2020-05-04 21:33:53 +0300

    logo change

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -17,7 +17,7 @@ header('Cache-Control: max-age=900');
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Favicon-->
-	<link rel="icon" href="<?php echo base_url(); ?>assets/img/icon.ico" type="image/icon" />
+	<link rel="icon" href="<?php echo base_url(); ?>assets/img/icon.png" type="image/x-icon">
 	<!-- Author Meta -->
 	<meta name="author" content="U70XDN">
 	<!-- Meta Description -->
@@ -47,7 +47,7 @@ header('Cache-Control: max-age=900');
 	<div class="container">
 		<div class="row align-items-center justify-content-between d-flex">
 			<div id="logo">
-				<a href="<?php echo base_url(); ?>home "><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="" title="" /></a>
+				<a href="<?php echo base_url(); ?>home "><img src="<?php echo base_url(); ?>assets/img/logo.jpg" alt="" title="" /></a>
 			</div>
 			<nav id="nav-menu-container">
 				<ul class="nav-menu">
